242a93ab6c6da42b929dc3a67385acf7d06b442f
CarlaGreim <carlagreim@gmail.com>
2022-07-23 23:21:36 -0300

    exercicios 1 ao 7 - concluidos

--- Semana 4/Exercicios 1 ao 7 - conta bancária/index.js
+++ Semana 4/Exercicios 1 ao 7 - conta bancária/index.js
@@ -2,34 +2,172 @@ let contasClientes = [
   {
     id: 1,
     nome: 'Cliente 01',
-    saldo: 500
+    saldo: 500,
+    senha: '1234'
   },
   {
     id: 2,
     nome: 'Cliente 02',
-    saldo: 3000
+    saldo: 3000,
+    senha: '12345'
   },
   {
     id: 3,
     nome: 'Cliente 03',
-    saldo: 5000
+    saldo: 5000,
+    senha: '123456'
   }
 ]
+const MESSAGE_TYPE = {
+  ERROR: 'ERROR',
+  SUCCESS: 'SUCCESS'
+}
+
+// Cria contante para armazenar as operacões
+const OPERACAO_CONTA = {
+  SACAR: 1,
+  DEPOSITAR: 2
+}
 
 const selectConta = document.getElementById('conta')
+const strongMensagem = document.getElementById('mensagem')
+const fomr = document.getElementById('form')
+const body = document.body
 
+const adicionarOpcao = (value, text) => {
+  const option = document.createElement('option')
+
+  option.value = value
+  option.textContent = text
+
+  selectConta.appendChild(option)
+}
 const populaSelect = () => {
   selectConta.innerHTML = ''
 
-  const option = document.createElement('option')
-  option.value = '0'
-  option.textContent = '(Selecione)'
-
-  selectConta.appendChild(option)
+  adicionarOpcao('0', '(Selecione)')
 
   contasClientes.forEach(conta => {
-    const option = document.createElement('option')
-    option.value = conta.id
-    option.textContent = conta.nome
+    adicionarOpcao(conta.id, conta.nome)
   })
 }
+body.onload = populaSelect
+
+const exibirMensagem = (mensagem, type = MESSAGE_TYPE.ERROR) => {
+  strongMensagem.textContent = mensagem
+
+  strongMensagem.className = type === MESSAGE_TYPE.ERROR ? 'error' : 'success'
+}
+
+const validarValor = valor => {
+  if (isNaN(valor) || valor <= 0) {
+    // exibirMensagem('valor inválido.')
+    return false
+  }
+
+  return true
+}
+const atualizaSaldo = (contaAtual, saldo) => {
+  const contasSemContaAtual = contasClientes.filter(c => c.id !== contaAtual.id)
+
+  const contaAtualizada = { ...contaAtual, saldo }
+
+  const contasAtualizadas = [...contasSemContaAtual, contaAtualizada]
+
+  contasClientes = contasAtualizadas
+}
+
+const validarSaldo = (valor, saldo) => {
+  if (valor > saldo) {
+    return false
+  }
+
+  return true
+}
+
+const obterConta = contaId => {
+  return contasClientes.find(conta => conta.id === contaId)
+}
+
+const sacar = (contaAtual, valor) => {
+  if (!validarValor(valor)) {
+    exibirMensagem('Valor inválido. Verifique e tente novamente!')
+    return
+  }
+
+  if (!validarSaldo(valor, contaAtual.saldo)) {
+    exibirMensagem(
+      `Saldo insuficiente, (lembre-se: você é pobre!) Saldo atual: ${contaAtual.saldo}`
+    )
+    return
+  }
+
+  const novoSaldo = contaAtual.saldo - valor
+
+  atualizaSaldo(contaAtual, novoSaldo)
+
+  exibirMensagem(
+    `Saque efetuado com sucesso! Saldo atual: ${novoSaldo}.`,
+    MESSAGE_TYPE.SUCCESS
+  )
+}
+
+const depositar = (contaAtual, valor) => {
+  if (!validarValor(valor)) {
+    exibirMensagem('Valor inválido. Verifique e tente novamente!')
+    return
+  }
+
+  const novoSaldo = contaAtual.saldo + valor
+  atualizaSaldo(contaAtual, novoSaldo)
+  exibirMensagem(
+    `Deposito efetuado com sucesso! Saldo atual: ${novoSaldo}.`,
+    MESSAGE_TYPE.SUCCESS
+  )
+}
+const realizarOperacao = event => {
+  event.preventDefault()
+
+  const contaId = parseInt(event.target.conta.value)
+  const valor = parseFloat(event.target.valor.value)
+  const operacao = parseInt(event.target.operacao.value)
+  const senha = event.target.senha.value
+
+  if (
+    !validarValor(contaId) ||
+    !validarValor(valor) ||
+    !validarValor(operacao)
+  ) {
+    exibirMensagem('Campos inválidos. Verifique e tente novamente.')
+    return
+  }
+  if (!senha) {
+    exibirMensagem('Informe a senha')
+    return
+  }
+
+  const contaAtual = obterConta(contaId)
+
+  if (!contaAtual) {
+    exibirMensagem('Conta não encontrada. Verifique e tente novamente.')
+    return
+  }
+
+  if (contaAtual.senha !== senha) {
+    exibirMensagem('Senha inválida!')
+    return
+  }
+
+  switch (operacao) {
+    case OPERACAO_CONTA.SACAR:
+      sacar(contaAtual, valor)
+      break
+    case OPERACAO_CONTA.DEPOSITAR:
+      depositar(contaAtual, valor)
+      break
+    default:
+      exibirMensagem('Operação inválida. Verifique e tente novamente.')
+      break
+  }
+}
+form.onsubmit = realizarOperacao
